e6713bee54592ad60649d29ae3b5a4cc4e05233e
ChunghwaMC <92585552+ChunghwaMC@users.noreply.github.com>
2025-02-23 00:14:02 +0800

    add folia supported (java minversion 21 to 17)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("dev.folia:folia-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("dev.folia:folia-api:1.20.4-R0.1-SNAPSHOT")
 
     implementation(files("./libs/config.jar"))
 
