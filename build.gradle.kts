82f0ae37368f0e210f0874833a6aeb73c09fb66b
ChunghwaMC <92585552+ChunghwaMC@users.noreply.github.com>
2025-02-22 23:54:25 +0800

    add folia supported

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,11 @@ version = ""
 repositories {
     mavenCentral()
 
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.16.1-R0.1-SNAPSHOT")
+    compileOnly("dev.folia:folia-api:1.21.4-R0.1-SNAPSHOT")
 
     implementation(files("./libs/config.jar"))
 
